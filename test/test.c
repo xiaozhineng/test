dcd9032b9b145898f9230408e89f50b6b7f82b4a
xiaozhineng <xiaozhineng0817@gmail.com>
2023-07-23 16:33:47 +0800

    test

---
+++ test/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "max.h"
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+    int c = 0;
+
+    c = max(a, b);
+    printf("hello word !!!\n");
+    printf("c = %d \n", c);
+
+    return 0;
+}
